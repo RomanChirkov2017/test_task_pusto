4c3b354484609bdac5b7cdd134110b3ee89707ab
RomanChirkov2017 <roman_chirkov_2017@mail.ru>
2024-09-07 18:53:45 +1000

    test task is done

--- game_profile/models.py
+++ game_profile/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Player(models.Model):
     username = models.CharField(max_length=250, unique=True, verbose_name='Имя пользователя')
     first_login = models.DateTimeField(auto_now_add=True, verbose_name='Дата регистрации пользователя')
-    points = models.IntegerField(default=0)
+    points = models.IntegerField(default=0, verbose_name='Баллы пользователя')
 
     def __str__(self):
         return self.username
